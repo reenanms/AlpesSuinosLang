90fe7287933341b7123d05e9faceb6537ee224a9
Renan Martins <renan.silva@ska.com.br>
2020-09-12 17:01:57 -0300

    Criado o projeto em .netcore onde será feito o interpretador de código ASL

---
+++ ASLang.Executor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ASLang.Executor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
